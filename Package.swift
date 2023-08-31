dffe5c936538ee5b7194c334b407abbb1df30e8b
hexagram <xml.hexagram@gmail.com>
2023-08-31 22:46:51 +0800

    fix

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,15 +15,15 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Gifski",
-            dependencies: ["libgifski2"]),
         .binaryTarget(
-            name: "libgifski2",
+            name: "libgifski",
             path: "./Gifski.xcframework"
         ),
+        .target(
+            name: "Gifski",
+            dependencies: ["libgifski"]),
         .testTarget(
             name: "GifskiTests",
-            dependencies: ["libgifski2"]),
+            dependencies: ["libgifski"]),
     ]
 )
